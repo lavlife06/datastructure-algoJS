107c727de39c1b86763405ca7e11cfe94e309abb
lavlife06 <lav7990192369@gmail.com>
2020-09-04 11:25:04 +0530

    added insert method

--- SinglylinkedList/Singly_Linked_List_allmethods.js
+++ SinglylinkedList/Singly_Linked_List_allmethods.js
@@ -87,6 +87,20 @@ class SinglyLinkedList {
     }
     return false;
   }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    var newNode = new Node(val);
+    var prev = this.get(index - 1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
 }
 
 var list = new SinglyLinkedList();
