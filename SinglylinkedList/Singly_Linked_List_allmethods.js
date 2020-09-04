6043ffed7c98c484472e552bd4b8a66f7d1d2e27
lavlife06 <lav7990192369@gmail.com>
2020-09-04 11:24:09 +0530

    added set method

--- SinglylinkedList/Singly_Linked_List_allmethods.js
+++ SinglylinkedList/Singly_Linked_List_allmethods.js
@@ -78,6 +78,15 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  set(index, val) {
+    var foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 var list = new SinglyLinkedList();
