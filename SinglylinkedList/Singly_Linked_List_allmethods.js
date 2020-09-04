d12132d11afa54cd14eafcbbd2cf017f4b40c77c
lavlife06 <lav7990192369@gmail.com>
2020-09-04 11:22:26 +0530

    added unshift method

--- SinglylinkedList/Singly_Linked_List_allmethods.js
+++ SinglylinkedList/Singly_Linked_List_allmethods.js
@@ -55,6 +55,18 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 var list = new SinglyLinkedList();
