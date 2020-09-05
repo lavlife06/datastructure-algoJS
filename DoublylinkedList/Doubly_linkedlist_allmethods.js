760e2413c3ef399333f5f675e9a25abf3c37a624
lavlife06 <lav7990192369@gmail.com>
2020-09-05 08:56:44 +0530

    added pop method in dll

--- DoublylinkedList/Doubly_linkedlist_allmethods.js
+++ DoublylinkedList/Doubly_linkedlist_allmethods.js
@@ -25,6 +25,21 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
 
 let list = new DoublyLinkedList();
